fbf875265b0ff5f869bfaf61782debf5d5e89aa0
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-03 14:09:21 -0500

    update config

---
+++ src/main/java/Config.java
@@ -0,0 +1,18 @@
+public class Config {
+
+    // METH
+    public String getURL(){
+        return "jdbc:mysql://localhost:3306/code_test_db?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+
+    public String getUser(){
+        return "root";
+    }
+
+    public String getPassword(){
+        return "codeup-1";
+    }
+
+
+
+}
