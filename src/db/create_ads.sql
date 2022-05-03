e0ed451278f7b80ff09d41b8acaa73e8261bd350
Jason Cameron <dev.jasoncameron@gmail.com>
2022-05-03 16:00:04 -0500

    start

---
+++ src/db/create_ads.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS adLister_db;
+-- pluralizing the name of the table
+USE adLister_db;
+
+TRUNCATE ads;
+
+-- 3. Write some SQL to drop a table named albums if it exists.
+DROP TABLE IF EXISTS ads;
+
+# Create an albums table with the following columns:
+CREATE TABLE IF NOT EXISTS ads  (
+                                      #       unsigned,NOT SIGNED , no negative values!
+                                      PRIMARY KEY (id),
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    title VARCHAR(100),
+    description VARCHAR(500)
+    );
+
+
+
+INSERT INTO ads (id, user_id, title, description)
+VALUES (1, 4, 'Playstation 590', 'So new it has not even come out yet'),
+       (2, 3, 'Moon Rock', 'From the lunar moon landing... For real!'),
+       (3, 2, 'Sand Bags', 'Everyone needs a few in their lives'),
+       (4, 1, 'Willy Wonka Ticket', 'Slightly used...')
+;
+
+DESC ads;
+
+SHOW CREATE TABLE ads;
+
+SELECT * FROM ads;
+
+
+
---
+++ src/db/create_users.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS adLister_db;
+-- pluralizing the name of the table
+USE adLister_db;
+
+TRUNCATE users;
+
+-- 3. Write some SQL to drop a table named albums if it exists.
+DROP TABLE IF EXISTS users;
+
+# Create an albums table with the following columns:
+CREATE TABLE IF NOT EXISTS users  (
+                                       #       unsigned,NOT SIGNED , no negative values!
+                                       PRIMARY KEY (id),
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    password VARCHAR(100)
+    );
+
+
+
+INSERT INTO users (id, username, email, password)
+VALUES (1, 'DarkSide', 'darkside@gmail.com', '12345'),
+       (2, 'MoonShot', 'moonshot@gmail.com', '12345'),
+       (3, 'DeathStar', 'deathstar@gmail.com', '12345'),
+       (4, 'WonderWall', 'wonderwall@gmail.com', '12345')
+     ;
+
+DESC users;
+
+SHOW CREATE TABLE users;
+
+SELECT * FROM users;
+
